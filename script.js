90e17d1e4884581d91ad194f9aba6633f9133d5f
Arminonly <amiaby2016@gmail.com>
2024-02-01 20:52:56 +0300

    dropdown

--- script.js
+++ script.js
@@ -1,11 +1,63 @@
-let close = document.querySelector('.modal__body-close');
-let modalWindow = document.querySelector('.modal');
-let burger = document.querySelector('.header__nav-burger');
+// * polifill
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function(cb, thisArg) {
+    thisArg = thisArg || window;
+    for (let i = 0; i < this.length; i++) {
+      cb.call(thisArg, this[i], i, this);
+    }
+  };
+}
 
-burger.addEventListener('click', () => {
-  modalWindow.style.display = 'block';
-});
+document.querySelectorAll('.dropdown').forEach(function(dropdownWrapper) {
+  const dropdownBtn = dropdownWrapper.querySelector('.dropdown__btn');
+  const dropdownList = dropdownWrapper.querySelector('.dropdown__list');
+  const dropdownListItem = dropdownList.querySelectorAll(
+    '.dropdown__list-item'
+  );
 
-close.addEventListener('click', () => {
-  modalWindow.style.display = 'none';
+  dropdownBtn.addEventListener('click', e => {
+    e.preventDefault();
+    dropdownList.classList.toggle('dropdown__list--visible');
+    dropdownBtn.classList.toggle('dropdown__btn-rotate-up');
+  });
+
+  dropdownListItem.forEach(el => {
+    el.addEventListener('click', function(e) {
+      e.stopPropagation();
+      dropdownBtn.innerText = this.innerText;
+      dropdownList.classList.remove('dropdown__list--visible');
+      dropdownBtn.classList.remove('dropdown__btn-rotate-up');
+    });
+  });
+
+  document.addEventListener(
+    'click',
+    e =>
+      e.target !== dropdownBtn
+        ? dropdownList.classList.remove('dropdown__list--visible')
+        : null
+  );
+
+  document.addEventListener(
+    'click',
+    e =>
+      e.target !== dropdownBtn
+        ? dropdownBtn.classList.remove('dropdown__btn-rotate-up')
+        : null
+  );
+
+  document.addEventListener(
+    'keydown',
+    e =>
+      e.key === 'Escape'
+        ? dropdownList.classList.remove('dropdown__list--visible')
+        : null
+  );
+  document.addEventListener(
+    'keydown',
+    e =>
+      e.key === 'Escape'
+        ? dropdownBtn.classList.remove('dropdown__btn-rotate-up')
+        : null
+  );
 });
